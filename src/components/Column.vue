5f85743c6d539bf7912a87e493a3dc5da6d990b1
renelimadev <renelima.dev@gmail.com>
2018-11-02 11:25:41 -0700

    Column.vue added

---
+++ src/components/Column.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div columnas
+</template>
+
+<script>
+export default {
+  name: 'column',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+</style>
